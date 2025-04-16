6447e0e4506a82649772691f90a039ace96b39f4
Bhupesh-Khordia <143842550+Bhupesh-Khordia@users.noreply.github.com>
2025-04-16 17:42:07 +0530

    Update pivot.cpp

--- L14/pivot.cpp
+++ L14/pivot.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int pivot(int arr[], int size) {
+int pivot(int *arr, int size) {
     int start = 0;
-    int end = size-1;
-    int mid = start + ((end - start)/2);
-                                            
-    while(start < end) {
-        if(arr[mid] >= arr[start]) {
-            start = start + 1;
-        }
-        else {
+    int end = size - 1;
+
+    while (start < end) {
+        int mid = start + (end - start) / 2;
+        if (arr[mid] > arr[end]) {
+            start = mid + 1;
+        } else {
             end = mid;
         }
-        mid = start + ((end - start)/2);
     }
-    return mid;
+    return start;
 }
 
 int main() {
